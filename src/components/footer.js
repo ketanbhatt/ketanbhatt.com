e49aeaf3d5c18d9e300f2deb82607a415c09ead4
Ketan Bhatt <ketanbhatt1006@gmail.com>
2020-04-12 21:09:30 +0100

    Add profile pic and footer

---
+++ src/components/footer.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import { rhythm } from '../utils/typography';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer
+        style={{
+          marginTop: rhythm(0.5),
+          paddingTop: rhythm(0.5),
+        }}
+      >
+        <div style={{ float: 'right' }}>
+          <a href="/rss.xml" target="_blank" rel="noopener noreferrer">
+            rss
+          </a>
+        </div>
+        <a
+          href="https://mobile.twitter.com/_ketanbhatt"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          twitter
+        </a>{' '}
+        &bull;{' '}
+        <a
+          href="https://github.com/ketanbhatt"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          github
+        </a>{' '}
+        &bull;{' '}
+        <a
+          href="https://www.linkedin.com/in/bhattketan/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          linkedin
+        </a>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,21 +1,59 @@
 import React from "react"
-import { Link } from "gatsby"
+import { useStaticQuery, Link } from "gatsby"
+import Image from "gatsby-image"
 
 import { rhythm, scale } from "../utils/typography"
 
+import Footer from "../components/footer"
+
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
+
+  const data = useStaticQuery(graphql`
+    query BioQuery {
+      avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
+        childImageSharp {
+          fixed(width: 60, height: 60) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+      site {
+        siteMetadata {
+          author {
+            name
+          }
+        }
+      }
+    }
+  `)
+
+  const author = data.site.siteMetadata.author
+
   let header
 
   if (location.pathname === rootPath) {
     header = (
       <h1
         style={{
-          ...scale(1.5),
-          marginBottom: rhythm(1.5),
+          ...scale(1.42),
+          marginBottom: 0,
           marginTop: 0,
         }}
       >
+        <Image
+          fixed={data.avatar.childImageSharp.fixed}
+          alt={author.name}
+          style={{
+            marginRight: rhythm(1 / 2),
+            marginBottom: 0,
+            minWidth: 50,
+            borderRadius: `100%`,
+          }}
+          imgStyle={{
+            borderRadius: `50%`,
+          }}
+        />
         <Link
           style={{
             boxShadow: `none`,
@@ -47,6 +85,7 @@ const Layout = ({ location, title, children }) => {
       </h3>
     )
   }
+
   return (
     <div
       style={{
@@ -58,11 +97,7 @@ const Layout = ({ location, title, children }) => {
     >
       <header>{header}</header>
       <main>{children}</main>
-      <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.org">Gatsby</a>
-      </footer>
+      <Footer />
     </div>
   )
 }
