026ce9b6a35614a5699b5e992c915f4629791776
Ketan Bhatt <ketanbhatt1006@gmail.com>
2020-04-15 10:06:33 +0100

    Add sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -91,6 +91,7 @@ module.exports = {
         app_id: 'lxmgmh8d',
       }
     },
+    `gatsby-plugin-sitemap`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
