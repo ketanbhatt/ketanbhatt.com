ebc9ba4993f29c0f7670fa29a089c28738ae9b89
Ketan Bhatt <ketanbhatt1006@gmail.com>
2020-04-20 08:50:29 +0100

    Add related posts to 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,17 +1,34 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { rhythm, scale } from "../utils/typography"
+
 
 const NotFoundPage = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
+  const relatedPosts = data.allMarkdownRemark.edges
+
+  const relatedPostsHtml = (
+    <div>
+      <h4>But all hope is not lost. Try these maybe...</h4>
+      <ul style={{paddingLeft: rhythm(1), marginLeft: 0}}>
+        {relatedPosts.map(({ node }) => {
+          return (<li style={{marginBottom: rhythm(1/5)}}><Link to={node.fields.slug}>{node.frontmatter.title}</Link></li>)
+        })}
+      </ul>
+      <hr/>
+    </div>
+  )
+
 
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title="404: Not Found" />
       <h1>Not Found</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      {relatedPostsHtml}
     </Layout>
   )
 }
@@ -23,6 +40,21 @@ export const pageQuery = graphql`
     site {
       siteMetadata {
         title
+      }
+    }
+    allMarkdownRemark(
+        sort: {fields: frontmatter___date, order: DESC},
+        limit: 5,
+    ){
+      edges {
+        node {
+          frontmatter {
+            title
+          }
+          fields {
+            slug
+          }
+        }
       }
     }
   }
