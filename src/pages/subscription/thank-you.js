3ce0a6ca17138cdcb5cdd6b6dbab8b2a4bdded85
Ketan Bhatt <ketanbhatt1006@gmail.com>
2020-05-02 16:47:23 +0100

    Update gatsby and add related posts to thank you

--- src/pages/subscription/thank-you.js
+++ src/pages/subscription/thank-you.js
@@ -1,17 +1,34 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../../components/layout"
 import SEO from "../../components/seo"
+import { rhythm } from "../../utils/typography"
+
 
 const ThankYouPage = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
+  const relatedPosts = data.allMarkdownRemark.edges
+
+  const relatedPostsHtml = (
+    <div>
+      <h4>Or, start reading something right now!</h4>
+      <ul style={{paddingLeft: rhythm(1), marginLeft: 0}}>
+        {relatedPosts.map(({ node }) => {
+          return (<li style={{marginBottom: rhythm(1/5)}}><Link to={node.fields.slug}>{node.frontmatter.title}</Link></li>)
+        })}
+      </ul>
+      <hr/>
+    </div>
+  )
 
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title="Subscription confirmed!" />
       <h1>Thank You!</h1>
-      <p>You're officially confirmed and on the list. Expect some great emails headed your way very soon.</p>
+      <p>You're officially confirmed and on the list.</p>
+      <p>Expect some great emails headed your way very soon.</p>
+      {relatedPostsHtml}
     </Layout>
   )
 }
@@ -23,6 +40,22 @@ export const pageQuery = graphql`
     site {
       siteMetadata {
         title
+      }
+    }
+    allMarkdownRemark(
+        filter: {frontmatter: {draft: {ne: true}}},
+        sort: {fields: frontmatter___date, order: DESC},
+        limit: 5,
+    ){
+      edges {
+        node {
+          frontmatter {
+            title
+          }
+          fields {
+            slug
+          }
+        }
       }
     }
   }
