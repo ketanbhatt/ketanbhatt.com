aeb23bbedcc427e601332024b5110aa531025280
Ketan Bhatt <ketanbhatt1006@gmail.com>
2020-05-09 12:43:44 +0100

    Add padding after every article in index

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ const BlogIndex = ({ data, location }) => {
       {posts.map(({ node }) => {
         const title = node.frontmatter.title || node.fields.slug
         return (
-          <article key={node.fields.slug}>
+          <article key={node.fields.slug} style={{paddingBottom: rhythm(0.5)}}>
             <header>
               <h3
                 style={{
